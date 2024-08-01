c48c73a8c233cece42bdf3cab259fe746fbd021b
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-08-01 16:35:44 +0200

    feat: enable a proxy server if config file is present

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
+import { loadConfig, ProxyServer } from '@cloud-cli/proxy';
 import { IncomingMessage, ServerResponse, createServer } from 'http';
 
 export default function (handler: (request: IncomingMessage, response: ServerResponse) => any) {
-  return createServer((req, res) => {
-    const end = res.end;
+  let server;
+  const config = loadConfig('', true);
 
-    res.end = (...args) => {
-      console.log(`[${new Date().toISOString().slice(0, 19)}] ${req.method} ${req.url} [${res.statusCode}]`);
-      return end.apply(res, args);
-    };
+  if (config) {
+    const proxy = new ProxyServer({
+      ...config,
+      enableDebug: true,
+      fallback: handler,
+    });
 
-    return handler(req, res);
-  }).listen(Number(process.env.PORT), () => {
+    server = createServer((req, res) => proxy.handleRequest(req, res, false));
+  } else {
+    server = createServer((req, res) => {
+      const end = res.end;
+
+      res.end = (...args) => {
+        console.log(`[${new Date().toISOString().slice(0, 19)}] ${req.method} ${req.url} [${res.statusCode}]`);
+        return end.apply(res, args);
+      };
+
+      return handler(req, res);
+    });
+  }
+
+  return server.listen(Number(process.env.PORT), () => {
     console.log(`Server started on port ${process.env.PORT}`);
   });
 }
